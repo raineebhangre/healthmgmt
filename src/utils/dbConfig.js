54896784d9b41d1018594dc7279a0ea014dc5bc4
raineebhangre <raineebhangre45@gmail.com>
2025-06-04 13:43:22 +0530

    Update dbConfig.js
    dbsse env

--- src/utils/dbConfig.js
+++ src/utils/dbConfig.js
@@ -3,7 +3,7 @@ import {drizzle} from 'drizzle-orm/neon-http'
 import * as schema from "./schema.js";  // ✅ Use "./" for same folder
 
 const sql = neon(
-   "postgresql://neondb_owner:npg_4LyzFYwfuTQ6@ep-withered-hat-a5gqryau-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require",
+   import.meta.env.VITE_DATABASE_URL,
 );  // ✅ Use Vite's env system
 
 export const db = drizzle(sql,{schema});
